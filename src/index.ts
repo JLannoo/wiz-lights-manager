1931761abceb7695822561071c003373bfdf40f7
JLannoo <julianlannoo@gmail.com>
2024-07-15 11:29:57 -0300

    Export SCENES in index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { WizLightManager } from "./manager";
 export { LocalCache } from "./cache/localCache";
+export { SCENES } from "./consts/scenes";
 
 export type { WizLight } from "./light";
 export type { LightGroup } from "./group";
